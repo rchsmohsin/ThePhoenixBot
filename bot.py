181de7f8f8baa6e4826c4ee1b56f4c660b22e4a8
rchsmohsin <mhaider@raleighcharterhs.org>
2018-02-17 22:00:30 -0500

    added dank handler

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 def start(bot, update):
   update.message.reply_text("I'm ThePhoenixBot, the (not so) official bot of the Phoenix Programing Club at RCHS. What's your name?")
   
+def start(bot, update):
+  update.message.reply_text("when you dank so hard but you don't succeed")
+  
 def convert_uppercase(bot, update):
   update.message.reply_text(update.message.text.upper())
 
@@ -13,8 +16,11 @@ def main():
   dispatcher = updater.dispatcher
   print("Bot started")
 
+  
   # Add command handler to dispatcher
   start_handler = CommandHandler('start',start)
+  dank_handler = CommandHandler('dank',dank)
+  
   upper_case = MessageHandler(Filters.text, convert_uppercase)
   dispatcher.add_handler(start_handler)
   dispatcher.add_handler(upper_case)
