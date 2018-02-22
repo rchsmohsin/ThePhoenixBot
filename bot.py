8015d30833ac9768a3269250a15b35d465830cf6
PM_ME_TOMATOES <agil@raleighcharterhs.org>
2018-02-22 15:06:42 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ def start(bot, update):
 def dank(bot, update):
   update.message.reply_text("when you dank so hard but you don't succeed")
   
+def hot(bot, update):
+  update.message.reply_text("man's not hot")
+
 def convert_uppercase(bot, update):
   update.message.reply_text(update.message.text.upper())
 
@@ -20,11 +23,13 @@ def main():
   # Add command handler to dispatcher
   start_handler = CommandHandler('start',start)
   dank_handler = CommandHandler('dank',dank)
+  hot_handler = CommandHandler ('hot',hot)
   
   upper_case = MessageHandler(Filters.text, convert_uppercase)
   dispatcher.add_handler(start_handler)
   dispatcher.add_handler(upper_case)
   dispatcher.add_handler(dank_handler)
+  dispatcher.add_handler(hot_handler)
   
   # Start the bot
   updater.start_polling()
